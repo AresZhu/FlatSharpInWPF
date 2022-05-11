8c7ab9905bcf40e38ebda0ceb0eb9dc7b0b58688
AresZhu <shranlove@hotmail.com>
2022-05-11 22:09:31 +1000

    wip

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+
+// Cannot import Models...
+using FlatSharpInWPF.Models;
+
+namespace FlatSharpInWPF
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
